bbe54a8d12f0ff0a193be2cf13fff5ede23a5110
Pankaj Rayal <pankajrayal@gmail.com>
2017-03-10 11:43:56 -0500

    Update 002

--- TypeScriptLearning/00_Initial/alpha.js
+++ TypeScriptLearning/00_Initial/alpha.js
@@ -1,5 +1,15 @@
-var Person123 = (function () {
-    function Person123() {
+var global_num = 12; //global variable
+var Numbers = (function () {
+    function Numbers() {
+        this.num_val = 13; //class variables
     }
-    return Person123;
+    Numbers.prototype.storeNum = function () {
+        var local_num = 14; //local variable
+    };
+    return Numbers;
 }());
+Numbers.sval = 10; //static field
+console.log('Global num:' + global_num);
+console.log(Numbers.sval); //static variables
+var obj = new Numbers();
+console.log("Global num: " + obj.num_val);
--- TypeScriptLearning/00_Initial/alpha.ts
+++ TypeScriptLearning/00_Initial/alpha.ts
@@ -1,3 +1,14 @@
-class Person123{
-    
+var global_num=12; //global variable
+class Numbers{
+    num_val = 13; //class variables
+    static sval = 10; //static field
+
+    storeNum():void{
+        var local_num = 14; //local variable
+    }
 }
+
+console.log('Global num:' + global_num);
+console.log(Numbers.sval); //static variables
+var obj = new Numbers();
+console.log("Global num: " + obj.num_val);
